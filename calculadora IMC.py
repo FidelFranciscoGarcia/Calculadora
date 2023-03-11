8e2b37657f2e7cdcd90d9ecce968389e121cb7c8
Fide Garcia <126542429+FidelFranciscoGarcia@users.noreply.github.com>
2023-03-11 01:14:30 -0600

    Add files via upload

--- calculadora IMC.py
+++ calculadora IMC.py
@@ -1,15 +1,28 @@
 
-nombre = (input("Por favor introduce tu nombre ") )
+nombre = str (input("Por favor introduce tu  nombre "))
+
+while nombre == "":
+    print ("No se puede dejar en blanco")
+    nombre = input ("por Introduce tu nombre ")  
 
 aPaterno= str (input ("Por favor introduce apellido paterno ")) 
 
+while aPaterno == "":
+    print ("No se puede dejar en blanco")
+    aPaterno = input ("Por Introduce tu apellido paterno ") 
+
 aMaterno = str (input ("Por favor introduce apellido materno "))
 
-edad =  int (input ("por favor ingrese su edad en años: "))
+while aMaterno == "":
+    print ("No se puede dejar en blanco")
+    aMaterno = input ("por Introduce apellido materno ")
 
-peso = float (input ("Por favor introduce tu peso ")) 
+edad = (input ("Por favor ingrese su edad en años "))
 
-estatura = float (input ("Por favor introduce tu estatura "))
+peso = float(input ("Por favor introduce tu peso "))
+
+estatura = float(input ("Por favor introduce tu estatura "))
+
 indice = peso / estatura ** 2
 
 print (f"Su indice de masa corporal es de: {indice}")
